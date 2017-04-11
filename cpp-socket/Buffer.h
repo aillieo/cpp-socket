25ad36ec455f7c89a915f270566b216050f62872
aillieo <aillieo@163.com>
2017-04-11 18:16:05 +0800

    change directory

--- cpp-socket/Buffer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Buffer
-{
-public:
-	Buffer(void);
-	~Buffer(void);
-};
-
--- cpp-socket/Config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-const int max_message_amount_display = 10;
-const int message_max_length = 1024;
-//const char server[20] = "192.168.103.142";
-const char server[20] = "127.0.0.1";
-const unsigned port = 9000;
-
